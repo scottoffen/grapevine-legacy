455526d4e1279339b5a215c0973a1497759dc872
Scott Offen <github@scottoffen.com>
2014-04-23 13:55:17 -0600

    Initial commit

--- SampleServer/Properties/AssemblyInfo.cs
+++ SampleServer/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SampleServer")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Sample server built on Grapevine")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Scott Offen")]
 [assembly: AssemblyProduct("SampleServer")]
