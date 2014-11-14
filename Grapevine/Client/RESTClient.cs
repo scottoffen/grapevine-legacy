d2c3c48927820b2ad5b55d98f8536853f87015e7
Ben Woodford <me@benwoodford.co.uk>
2014-11-14 16:19:18 +0000

    Moved ContentType below Header setting

--- Grapevine/Client/RESTClient.cs
+++ Grapevine/Client/RESTClient.cs
@@ -10,73 +10,67 @@ namespace Grapevine.Client
     /// </summary>
     public class RESTClient
     {
-        public RESTClient(string baseUrl)
+        public RESTClient (string baseUrl)
         {
-            this.BaseUrl = Regex.Replace(baseUrl, "/$", "");
-            this.Cookies = new CookieContainer();
+            this.BaseUrl = Regex.Replace (baseUrl, "/$", "");
+            this.Cookies = new CookieContainer ();
         }
 
         /// <summary>
         /// Sends RESTRequest to server represented by RESTClient and returns the RESTResponse received
         /// </summary>
-        public RESTResponse Execute(RESTRequest request)
+        public RESTResponse Execute (RESTRequest request)
         {
             var querystr = request.QueryString;
-            var url = (object.ReferenceEquals(querystr, null)) ? this.BaseUrl + "/" + request.PathInfo : this.BaseUrl + "/" + request.PathInfo + "?" + querystr;
-            var client = CreateRequest(url);
+            var url = (object.ReferenceEquals (querystr, null)) ? this.BaseUrl + "/" + request.PathInfo : this.BaseUrl + "/" + request.PathInfo + "?" + querystr;
+            var client = CreateRequest (url);
 
-            var stopwatch = new Stopwatch();
+            var stopwatch = new Stopwatch ();
             client.Timeout = request.Timeout;
 
             client.CookieContainer = this.Cookies;
-            client.Method = request.Method.ToString();
-            client.ContentType = request.ContentType.ToValue();
+            client.Method = request.Method.ToString ();
             client.Headers = request.Headers;
 
-            if (!Object.ReferenceEquals(request.Payload, null))
-            {
-                var content = request.Encoding.GetBytes(request.Payload);
+            client.ContentType = request.ContentType.ToValue ();
+
+            if (!Object.ReferenceEquals (request.Payload, null)) {
+                var content = request.Encoding.GetBytes (request.Payload);
                 client.ContentLength = content.Length;
-                client.GetRequestStream().Write(content, 0, content.Length);
-                client.GetRequestStream().Close();
-            }
-            else
-            {
+                client.GetRequestStream ().Write (content, 0, content.Length);
+                client.GetRequestStream ().Close ();
+            } else {
                 client.ContentLength = 0;
             }
-
+                
             HttpWebResponse httpresponse;
             string error = "";
             WebExceptionStatus errorStatus = WebExceptionStatus.Success;
 
-            stopwatch.Start();
-            try
-            {
-                httpresponse = (HttpWebResponse)client.GetResponse();
-            }
-            catch (WebException e)
-            {
-                EventLogger.Log(e);
+            stopwatch.Start ();
+            try {
+                httpresponse = (HttpWebResponse)client.GetResponse ();
+            } catch (WebException e) {
+                EventLogger.Log (e);
 
                 httpresponse = (HttpWebResponse)e.Response;
                 error = e.Message;
                 errorStatus = e.Status;
             }
-            stopwatch.Stop();
-            request.Reset();
+            stopwatch.Stop ();
+            request.Reset ();
 
-            var response = new RESTResponse(httpresponse, stopwatch.ElapsedMilliseconds, error, errorStatus);
-            this.Cookies.Add(response.Cookies);
+            var response = new RESTResponse (httpresponse, stopwatch.ElapsedMilliseconds, error, errorStatus);
+            this.Cookies.Add (response.Cookies);
 
             return response;
         }
 
-        private HttpWebRequest CreateRequest(string url)
+        private HttpWebRequest CreateRequest (string url)
         {
-            var request = (HttpWebRequest)WebRequest.Create(url);
+            var request = (HttpWebRequest)WebRequest.Create (url);
 
-            if (!Object.ReferenceEquals(this.NetworkCredentials, null))
-            {
+            if (!Object.ReferenceEquals (this.NetworkCredentials, null)) {
                 request.Credentials = this.NetworkCredentials;
             }
 
