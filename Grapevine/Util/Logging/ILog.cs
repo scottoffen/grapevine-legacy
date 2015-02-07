e493a187abf7969a1eee3fa7c6ced8f3d41cd57c
Daniel Möllenbeck <daniel.moellenbeck@ncr.com>
2015-02-07 17:10:16 +0100

    Replaced logger with more generic implementation

---
+++ Grapevine/Util/Logging/ILog.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Grapevine.Util.Logging
+{
+	public interface ILog
+	{
+		void Log(string message);
+		void Log(Exception ex);
+	}
+}
+
---
+++ Grapevine/Util/Logging/LogToConsole.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Grapevine.Server;
+
+namespace Grapevine.Util.Logging
+{
+	public class LogToConsole : Grapevine.Util.Logging.ILog
+	{
+		public LogToConsole ()
+		{
+		}
+
+		public void Log(string l)
+		{
+			System.Console.WriteLine (l);
+		}
+
+		public void Log(Exception ex)
+		{
+			System.Console.WriteLine (ex);
+		}
+	}
+}
+
