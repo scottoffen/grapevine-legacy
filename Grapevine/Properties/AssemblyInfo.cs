96657ba68a1c6abfd9d8910683e2cf0350582871
Tom Surace <toms@derby.com>
2015-11-05 11:37:15 -0700

    Created NuGet package for interspecific; added to build.

--- Grapevine/Properties/AssemblyInfo.cs
+++ Grapevine/Properties/AssemblyInfo.cs
@@ -5,22 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Grapevine")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Scott Offen")]
-[assembly: AssemblyProduct("Grapevine")]
-[assembly: AssemblyCopyright("© 2014 Scott Offen")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("f6ac4c2c-4801-465c-8dd4-932dc1890747")]
+[assembly: AssemblyTitle("Interspecific")]
+[assembly: AssemblyProduct("Interspecific")]
+[assembly: AssemblyDescription("Interspecific REST HTTP server framework. See https://github.com/tekhedd/Interspecific")]
+[assembly: AssemblyCompany("Interspecific project contributors")]
+[assembly: AssemblyCopyright("© 2014-2015 Scott Offen and Interspecific project contributors")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +21,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.0.4.0")]
-[assembly: AssemblyFileVersion("3.0.4.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
