e71d99a142ddd5398e1fc8f7767d085e6d257d16
Scott Offen <github@scottoffen.com>
2016-06-28 10:24:58 -0600

    Revert "4.0.0-alpha.0"

---
+++ Grapevine/Util/HttpMethod.cs
@@ -0,0 +1,25 @@
+﻿namespace Grapevine
+{
+    /// <summary>
+    /// Http methods (or verbs) to indicate the desired action to be performed on the resource
+    /// </summary>
+    public enum HttpMethod
+    {
+        CONNECT,
+        COPY,
+        DELETE,
+        GET,
+        HEAD,
+        LINK,
+        LOCK,
+        OPTIONS,
+        PATCH,
+        POST,
+        PROPFIND,
+        PURGE,
+        PUT,
+        TRACE,
+        UNLINK,
+        UNLOCK
+    };
+}
