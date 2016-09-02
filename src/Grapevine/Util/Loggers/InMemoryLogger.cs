57174a5c765beb920bf0d3a50e3c0c1c66bb8363
Scott Offen <github@scottoffen.com>
2016-09-02 10:25:28 -0600

    Added inheritdoc to Logger implementations

--- src/Grapevine/Util/Loggers/InMemoryLogger.cs
+++ src/Grapevine/Util/Loggers/InMemoryLogger.cs
@@ -8,10 +8,17 @@ namespace Grapevine.Util.Loggers
     /// </summary>
     public sealed class InMemoryLogger : IGrapevineLogger
     {
+        /// <inheritdoc/>
         public LogLevel Level { get; set; }
 
-        private static DateTime RightNow => DateTime.Now;
+        /// <summary>
+        /// A value that represents the current time
+        /// </summary>
+        public static DateTime RightNow => DateTime.Now;
 
+        /// <summary>
+        /// Contains all the messages that have been logged
+        /// </summary>
         public List<LogEvent> Logs { get; }
 
         public InMemoryLogger() : this(LogLevel.Trace){}
@@ -22,97 +29,116 @@ namespace Grapevine.Util.Loggers
             Logs = new List<LogEvent>();
         }
 
+        /// <inheritdoc/>
         public void Log(LogEvent evt)
         {
             if (evt.Level > Level) return;
             Logs.Add(evt);
         }
 
+        /// <inheritdoc/>
         public void Trace(object obj)
         {
             Log(new LogEvent { Level = LogLevel.Trace, DateTime = RightNow, Message = obj.ToString() });
         }
 
+        /// <inheritdoc/>
         public void Trace(string message)
         {
             Log(new LogEvent { Level = LogLevel.Trace, DateTime = RightNow, Message = message });
         }
 
+        /// <inheritdoc/>
         public void Trace(string message, Exception ex)
         {
             Log(new LogEvent { Level = LogLevel.Trace, DateTime = RightNow, Message = message, Exception = ex });
         }
 
+        /// <inheritdoc/>
         public void Debug(object obj)
         {
             Log(new LogEvent { Level = LogLevel.Debug, DateTime = RightNow, Message = obj.ToString() });
         }
 
+        /// <inheritdoc/>
         public void Debug(string message)
         {
             Log(new LogEvent { Level = LogLevel.Debug, DateTime = RightNow, Message = message });
         }
 
+        /// <inheritdoc/>
         public void Debug(string message, Exception ex)
         {
             Log(new LogEvent { Level = LogLevel.Debug, DateTime = RightNow, Message = message, Exception = ex });
         }
 
+        /// <inheritdoc/>
         public void Info(object obj)
         {
             Log(new LogEvent { Level = LogLevel.Info, DateTime = RightNow, Message = obj.ToString() });
         }
 
+        /// <inheritdoc/>
         public void Info(string message)
         {
             Log(new LogEvent { Level = LogLevel.Info, DateTime = RightNow, Message = message });
         }
 
+        /// <inheritdoc/>
         public void Info(string message, Exception ex)
         {
             Log(new LogEvent { Level = LogLevel.Info, DateTime = RightNow, Message = message, Exception = ex });
         }
 
+        /// <inheritdoc/>
         public void Warn(object obj)
         {
             Log(new LogEvent { Level = LogLevel.Warn, DateTime = RightNow, Message = obj.ToString() });
         }
 
+        /// <inheritdoc/>
         public void Warn(string message)
         {
             Log(new LogEvent { Level = LogLevel.Warn, DateTime = RightNow, Message = message });
         }
 
+        /// <inheritdoc/>
         public void Warn(string message, Exception ex)
         {
             Log(new LogEvent { Level = LogLevel.Warn, DateTime = RightNow, Message = message, Exception = ex });
         }
 
+        /// <inheritdoc/>
         public void Error(object obj)
         {
             Log(new LogEvent { Level = LogLevel.Error, DateTime = RightNow, Message = obj.ToString() });
         }
 
+        /// <inheritdoc/>
         public void Error(string message)
         {
             Log(new LogEvent { Level = LogLevel.Error, DateTime = RightNow, Message = message });
         }
 
+        /// <inheritdoc/>
         public void Error(string message, Exception ex)
         {
             Log(new LogEvent { Level = LogLevel.Error, DateTime = RightNow, Message = message, Exception = ex });
         }
 
+        /// <inheritdoc/>
         public void Fatal(object obj)
         {
             Log(new LogEvent { Level = LogLevel.Fatal, DateTime = RightNow, Message = obj.ToString() });
         }
 
+        /// <inheritdoc/>
         public void Fatal(string message)
         {
             Log(new LogEvent { Level = LogLevel.Fatal, DateTime = RightNow, Message = message });
         }
 
+        /// <inheritdoc/>
         public void Fatal(string message, Exception ex)
         {
             Log(new LogEvent { Level = LogLevel.Fatal, DateTime = RightNow, Message = message, Exception = ex });
--- src/Grapevine/Util/Loggers/NullLogger.cs
+++ src/Grapevine/Util/Loggers/NullLogger.cs
@@ -7,44 +7,64 @@ namespace Grapevine.Util.Loggers
     /// </summary>
     public sealed class NullLogger : IGrapevineLogger
     {
+        /// <inheritdoc/>
         public LogLevel Level => LogLevel.Trace;
 
+        /// <inheritdoc/>
         public void Log(LogEvent evt) { /* This method is intentionally left empty */ }
 
+        /// <inheritdoc/>
         public void Debug(string message) { /* This method is intentionally left empty */ }
 
+        /// <inheritdoc/>
         public void Debug(object obj) { /* This method is intentionally left empty */ }
 
+        /// <inheritdoc/>
         public void Debug(string message, Exception ex) { /* This method is intentionally left empty */ }
 
+        /// <inheritdoc/>
         public void Error(string message) { /* This method is intentionally left empty */ }
 
+        /// <inheritdoc/>
         public void Error(object obj) { /* This method is intentionally left empty */ }
 
+        /// <inheritdoc/>
         public void Error(string message, Exception ex) { /* This method is intentionally left empty */ }
 
+        /// <inheritdoc/>
         public void Fatal(string message) { /* This method is intentionally left empty */ }
 
+        /// <inheritdoc/>
         public void Fatal(object obj) { /* This method is intentionally left empty */ }
 
+        /// <inheritdoc/>
         public void Fatal(string message, Exception ex) { /* This method is intentionally left empty */ }
 
+        /// <inheritdoc/>
         public void Info(string message) { /* This method is intentionally left empty */ }
 
+        /// <inheritdoc/>
         public void Info(object obj) { /* This method is intentionally left empty */ }
 
+        /// <inheritdoc/>
         public void Info(string message, Exception ex) { /* This method is intentionally left empty */ }
 
+        /// <inheritdoc/>
         public void Trace(string message) { /* This method is intentionally left empty */ }
 
+        /// <inheritdoc/>
         public void Trace(object obj) { /* This method is intentionally left empty */ }
 
+        /// <inheritdoc/>
         public void Trace(string message, Exception ex) { /* This method is intentionally left empty */ }
 
+        /// <inheritdoc/>
         public void Warn(string message) { /* This method is intentionally left empty */ }
 
+        /// <inheritdoc/>
         public void Warn(object obj) { /* This method is intentionally left empty */ }
 
+        /// <inheritdoc/>
         public void Warn(string message, Exception ex) { /* This method is intentionally left empty */ }
     }
 }
