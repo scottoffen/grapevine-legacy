efa5048abc68f011eba82ee93a943c09df9e0023
Scott Offen <github@scottoffen.com>
2016-09-15 09:45:36 -0600

    Added unit tests for RestServer

---
+++ src/Grapevine.Tests/Server/Helpers/RestServerTesterHelper.cs
@@ -0,0 +1,20 @@
+﻿using Grapevine.Server;
+
+namespace Grapevine.Tests.Server.Helpers
+{
+    class RestServerTesterHelper
+    {
+    }
+
+    public class CustomSettings : ServerSettings
+    {
+        public CustomSettings()
+        {
+            Port = "5555";
+            OnBeforeStart = () =>
+            {
+                UseHttps = true;
+            };
+        }
+    }
+}
--- src/Grapevine.Tests/Server/RestServerTester.cs
+++ src/Grapevine.Tests/Server/RestServerTester.cs
@@ -1,32 +1,114 @@
-﻿using Xunit;
+﻿using System;
+using Xunit;
 using Shouldly;
 using Grapevine.Server;
+using Grapevine.Tests.Server.Helpers;
+using Grapevine.Util;
+using Grapevine.Util.Loggers;
 
 namespace Grapevine.Tests.Server
 {
     public class RestServerTester
     {
-        //[Fact]
-        //public void server_usehttps_is_true_then_listenerprefix_begins_with_https()
-        //{
-        //    var server = new RestServer();
-        //    server.ShouldNotBeNull();
+        [Fact]
+        public void server_default_configuration()
+        {
+            var server = new RestServer();
 
-        //    server.UseHttps = true;
+            server.Connections.ShouldBe(50);
+            server.EnableThrowingExceptions.ShouldBeFalse();
+            server.Host.ShouldBe("localhost");
+            server.IsListening.ShouldBeFalse();
+            server.ListenerPrefix.ShouldBe("http://localhost:1234/");
+            server.Logger.ShouldBeOfType<NullLogger>();
+            server.OnAfterStart.ShouldBeNull();
+            server.OnAfterStop.ShouldBeNull();
+            server.OnBeforeStart.ShouldBeNull();
+            server.OnBeforeStop.ShouldBeNull();
+            server.OnStart.ShouldBeNull();
+            server.OnStop.ShouldBeNull();
+            server.Port.ShouldBe("1234");
+            server.PublicFolder.ShouldBeOfType<PublicFolder>();
+            server.Router.ShouldBeOfType<Router>();
+            server.UseHttps.ShouldBeFalse();
+        }
 
-        //    server.UseHttps.ShouldBeTrue();
-        //    server.ListenerPrefix.StartsWith("https").ShouldBeTrue();
-        //}
+        [Fact]
+        public void server_on_stop_and_stop_synonyms()
+        {
+            Action action = () => { var x = 2; };
 
-        //[Fact]
-        //public void server_usehttps_is_false_then_listenerprefix_begins_with_http()
-        //{
-        //    var server = new RestServer();
-        //    server.ShouldNotBeNull();
+            var server = new RestServer();
 
-        //    server.UseHttps.ShouldBeFalse();
-        //    server.ListenerPrefix.StartsWith("http").ShouldBeTrue();
-        //    server.ListenerPrefix.StartsWith("https").ShouldBeFalse();
-        //}
+            server.OnAfterStart = action;
+            server.OnStart.ShouldBe(action);
+
+            server.OnStart = null;
+            server.OnAfterStart.ShouldBeNull();
+
+            server.OnAfterStop = action;
+            server.OnStop.ShouldBe(action);
+
+            server.OnStop = null;
+            server.OnAfterStop.ShouldBeNull();
+        }
+
+        [Fact]
+        public void server_configures_using_for_generic_type()
+        {
+            var server = RestServer.For<CustomSettings>();
+            server.Port.ShouldBe("5555");
+        }
+
+        [Fact]
+        public void server_configures_using_action()
+        {
+            const string port = "5555";
+            const string index = "default.htm";
+
+            var server = RestServer.For(_ =>
+            {
+                 _.Port = port;
+                _.PublicFolder.DefaultFileName = index;
+            });
+
+            server.Port.ShouldBe(port);
+            server.PublicFolder.DefaultFileName.ShouldBe(index);
+        }
+
+        [Fact]
+        public void server_listenerprefix_begins_with_https_when_usehttps_is_true()
+        {
+            var server = new RestServer();
+            server.ShouldNotBeNull();
+
+            server.UseHttps = true;
+
+            server.UseHttps.ShouldBeTrue();
+            server.ListenerPrefix.StartsWith("https").ShouldBeTrue();
+        }
+
+        [Fact]
+        public void server_listenerprefix_begins_with_http_when_usehttps_is_false()
+        {
+            var server = new RestServer();
+            server.ShouldNotBeNull();
+
+            server.UseHttps.ShouldBeFalse();
+            server.ListenerPrefix.StartsWith("http").ShouldBeTrue();
+            server.ListenerPrefix.StartsWith("https").ShouldBeFalse();
+        }
+
+        [Fact]
+        public void server_starts_and_stops()
+        {
+            var server = new RestServer();
+
+            server.Start();
+            server.IsListening.ShouldBeTrue();
+
+            server.Stop();
+            server.IsListening.ShouldBeFalse();
+        }
     }
 }
