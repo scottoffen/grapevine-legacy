c6e6c908852bd8f28c9655ad26df4e1253ab1762
Scott Offen <github@scottoffen.com>
2016-10-05 19:45:32 -0600

    Added message-only constructor

--- src/Grapevine/Exceptions/Server/UnableToStopHostException.cs
+++ src/Grapevine/Exceptions/Server/UnableToStopHostException.cs
@@ -7,6 +7,8 @@ namespace Grapevine.Exceptions.Server
     /// </summary>
     public class UnableToStopHostException : Exception
     {
+        public UnableToStopHostException(string message) : base(message) { }
+
         public UnableToStopHostException(string message, Exception inner) : base(message, inner) { }
     }
 }
