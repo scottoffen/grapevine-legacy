8448914a315ca9afa32f2e89bf501236703383c2
Scott Offen <github@scottoffen.com>
2016-11-16 14:30:55 -0700

    Updated version number in AssemblyInfo

--- src/Grapevine/Properties/AssemblyInfo.cs
+++ src/Grapevine/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.0.0.185")]
-[assembly: AssemblyFileVersion("4.0.0.187")]
+[assembly: AssemblyVersion("4.0.0.194")]
+[assembly: AssemblyFileVersion("4.0.0.194")]
 
 [assembly: InternalsVisibleTo("Grapevine.Tests")]
